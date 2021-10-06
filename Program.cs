f6147fb9ea423c76359887625bcfa20c839cc53a
Miirakulous <olugbanmu@outlook.com>
2021-10-06 12:50:55 -0400

    Created Rectangle Class with 7 Methods and 2 Constructors

--- Program.cs
+++ Program.cs
@@ -1,15 +1,76 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RectangularPrism
 {
     class Program
     {
+        private int length;
+        private int width;
+        private int height;
         static void Main(string[] args)
         {
+            Rectangle RectPrism = new Rectangle();
+            Console.WriteLine("1- Get Rectangle Length");
+            Console.WriteLine("2- Change Rectangle Length");
+            Console.WriteLine("3- Get Rectangle Width");
+            Console.WriteLine("4- Change Rectangle Width");
+            Console.WriteLine("5 - Get Rectangle Height");
+            Console.WriteLine("6- Change Rectangle Height");
+            Console.WriteLine("7- Get Rectangle Volume");
+            Console.WriteLine("8- Exit");
+
+
+            Console.WriteLine("Please select an option to get started");
+            string uInput = Console.ReadLine();
+            if (uInput != "1" ||
+                uInput != "2" ||
+                uInput != "3" ||
+                uInput != "4" ||
+                uInput != "5" ||
+                uInput != "6" ||
+                uInput != "7" ||
+                uInput != "8"
+                )
+            {
+                Console.WriteLine("Please select a valid option");
+            }
+
+            #region Switch Statement
+            switch (uInput)
+            {
+                case "1":
+                    Console.WriteLine("The Length of your rectangle is: " + RectPrism.GetLength());
+                    break;
+                case "2":
+                    Console.WriteLine("Enter the new length of your Rectangle: ");
+                    int length = RectPrism.SetLength(int.Parse(Console.ReadLine()));
+                    break;
+                case "3":
+                    Console.WriteLine("The Width of your rectangle is: " + RectPrism.GetWidth());
+                    break;
+                case "4":
+                    Console.WriteLine("Enter the new width of your Rectangle: ");
+                    int width = RectPrism.SetWidth(int.Parse(Console.ReadLine()));
+                    break;
+                case "5":
+                    Console.WriteLine("The Height of your Rectangle is: " + RectPrism.GetHeight());
+                    break;
+                case "6":
+                    Console.WriteLine("Enter the new height of your Rectangle: ");
+                    int height = RectPrism.SetHeight(int.Parse(Console.ReadLine()));
+                    break;
+                case "7":
+                    Console.WriteLine("The Volume of your Rectangle is: " + RectPrism.GetVolume());
+                    break;
+                case "8":
+                    Console.WriteLine("Exit...");
+                    break;
+                default:
+                    break;
+            }
+            #endregion
+
         }
+
     }
 }
---
+++ Rectangle.cs
@@ -0,0 +1,54 @@
+﻿namespace RectangularPrism
+{
+    class Rectangle
+    {
+        private int length;
+        private int width;
+        private int height;
+        public Rectangle()
+        {
+            this.length = 1;
+            this.width = 1;
+            this.height = 1;
+        }
+        public Rectangle(int length, int height, int width)
+        {
+            this.length = length;
+            this.height = height;
+            this.width = width;
+        }
+
+        public int GetLength()
+        {
+            return this.length;
+        }
+        public int GetHeight()
+        {
+            return this.height;
+        }
+        public int GetWidth()
+        {
+            return this.width;
+        }
+        public int SetLength(int length)
+        {
+            this.length = length;
+            return this.length;
+        }
+        public int SetWidth(int width)
+        {
+            this.width = width;
+            return this.width;
+        }
+        public int SetHeight(int height)
+        {
+            this.height = height;
+            return this.height;
+        }
+        public int GetVolume()
+        {
+            return this.length * this.width * this.height;
+        }
+    }
+
+}
